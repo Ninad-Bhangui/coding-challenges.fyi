7d24a91ca382d14670c46efe98581fda6e344271
Ninad Bhangui <b.ninad50@gmail.com>
2023-07-21 22:12:09 +0530

    individual options functional

--- 01/gowc/cmd/main.go
+++ 01/gowc/cmd/main.go
@@ -1,45 +1,118 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+type CliOptions struct {
+	shouldGetByteCount bool
+	shouldGetLineCount bool
+	shouldGetWordCount bool
+	shouldGetCharCount bool
+	fileNames          []string
+}
+
+func (cliOptions *CliOptions) noOptionsSetDefault() {
+	if !cliOptions.shouldGetByteCount && !cliOptions.shouldGetLineCount && !cliOptions.shouldGetWordCount && !cliOptions.shouldGetCharCount {
+		cliOptions.shouldGetByteCount = true
+		cliOptions.shouldGetLineCount = true
+		cliOptions.shouldGetWordCount = true
+	}
+}
 func main() {
 	shouldGetByteCount := flag.Bool("c", false, "Number of bytes in file or stdin")
+	shouldGetLineCount := flag.Bool("l", false, "Number of lines in file or stdin")
+	shouldGetWordCount := flag.Bool("w", false, "Number of words in file or stdin")
+	shouldGetCharCount := flag.Bool("m", false, "Number of characters in file or stdin")
 	flag.Parse()
+
 	nonFlagArgCount := flag.NArg()
 	fileNameIndex := len(os.Args) - nonFlagArgCount
 	fileNames := os.Args[fileNameIndex:]
-	err := cliEntryPoint(shouldGetByteCount, fileNames)
+
+	cliOptions := CliOptions{
+		shouldGetByteCount: *shouldGetByteCount,
+		shouldGetLineCount: *shouldGetLineCount,
+		shouldGetWordCount: *shouldGetWordCount,
+		shouldGetCharCount: *shouldGetCharCount,
+		fileNames:          fileNames,
+	}
+
+	err := cliEntryPoint(&cliOptions, os.Stdout)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func cliEntryPoint(shouldGetByteCount *bool, fileNames []string) error {
-	for i := range fileNames {
-		if *shouldGetByteCount {
-			byteCount, err := getByteCountOfFile(fileNames[i])
+func cliEntryPoint(cliOptions *CliOptions, output io.Writer) error {
+	cliOptions.noOptionsSetDefault()
+	for i := range cliOptions.fileNames {
+		if cliOptions.shouldGetByteCount {
+			byteCount, err := getByteCountOfFile(cliOptions.fileNames[i])
 			if err != nil {
 				return err
 			}
-			fmt.Fprintf(os.Stdout, "%d %s\n", byteCount, fileNames[i])
+			fmt.Fprintf(output, "%d %s\n", byteCount, cliOptions.fileNames[i])
 			return nil
+		} else if cliOptions.shouldGetLineCount {
+			lineCount, err := getLineCountOfFile(cliOptions.fileNames[i])
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(output, "%d %s\n", lineCount, cliOptions.fileNames[i])
+		} else if cliOptions.shouldGetWordCount {
+			lineCount, err := getWordCountOfFile(cliOptions.fileNames[i])
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(output, "%d %s\n", lineCount, cliOptions.fileNames[i])
+		} else if cliOptions.shouldGetCharCount {
+			lineCount, err := getCharCountOfFile(cliOptions.fileNames[i])
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(output, "%d %s\n", lineCount, cliOptions.fileNames[i])
 		}
+
 	}
 	return nil
 
 }
 
 func getByteCountOfFile(fileName string) (int, error) {
-	bytes, err := os.ReadFile(fileName)
-	// info, err := os.Stat(fileName)
-	if err != nil {
-		return 0, err
-	}
-	return len(bytes), nil
+	return getSplitCountFile(fileName, bufio.ScanBytes)
+}
 
+func getLineCountOfFile(fileName string) (int, error) {
+	return getSplitCountFile(fileName, bufio.ScanLines)
+}
+
+func getWordCountOfFile(fileName string) (int, error) {
+	return getSplitCountFile(fileName, bufio.ScanWords)
+}
+
+func getCharCountOfFile(fileName string) (int, error) {
+	return getSplitCountFile(fileName, bufio.ScanRunes)
+}
+
+func getSplitCountFile(fileName string, splitFunc bufio.SplitFunc) (int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return 0, nil
+	}
+	return getSplitCount(file, splitFunc)
+}
+func getSplitCount(input io.Reader, splitFunc bufio.SplitFunc) (int, error) {
+	count := 0
+	scanner := bufio.NewScanner(input)
+	scanner.Split(splitFunc)
+	for scanner.Scan() {
+		count++
+	}
+	return count, nil
 }
--- 01/gowc/cmd/main_test.go
+++ 01/gowc/cmd/main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -11,12 +14,50 @@ func TestCli(t *testing.T) {
 	// 		t.Errorf("got %q want %q", got, want)
 	// 	}
 	// }
-	t.Run("testing bytecount of sample file should not return error", func(t *testing.T) {
-		shouldGetByteCount := true
-		fileNames := []string{"../samples/gutenberg.org_cache_epub_132_pg132.txt"}
-		got := cliEntryPoint(&shouldGetByteCount, fileNames)
-		if got != nil {
-			t.Errorf("error should be nil, got: %s", got)
+	t.Run("testing bytecount of sample file return same output as wc -c fileName", func(t *testing.T) {
+		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
+		fileNames := []string{fileName}
+		cliOptions := CliOptions{
+			shouldGetByteCount: true,
+			shouldGetLineCount: false,
+			shouldGetWordCount: false,
+			shouldGetCharCount: false,
+			fileNames:          fileNames,
+		}
+		var buffer bytes.Buffer
+		testWriter := bufio.NewWriter(&buffer)
+		error := cliEntryPoint(&cliOptions, testWriter)
+		if error != nil {
+			t.Errorf("error should be nil, got: %s", error)
+		}
+		testWriter.Flush()
+		got := buffer.String()
+		want := fmt.Sprintf("341836 %s\n", fileName)
+		if got != want {
+			t.Errorf("cli output should be %s, got %s.", want, got)
+		}
+	})
+	t.Run("testing no options of sample file should behave as -c -l and -w", func(t *testing.T) {
+		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
+		fileNames := []string{fileName}
+		cliOptions := CliOptions{
+			shouldGetByteCount: false,
+			shouldGetLineCount: false,
+			shouldGetWordCount: false,
+			shouldGetCharCount: false,
+			fileNames:          fileNames,
+		}
+		var buffer bytes.Buffer
+		testWriter := bufio.NewWriter(&buffer)
+		error := cliEntryPoint(&cliOptions, testWriter)
+		if error != nil {
+			t.Errorf("error should be nil, got: %s", error)
+		}
+		testWriter.Flush()
+		got := buffer.String()
+		want := fmt.Sprintf("  7137  58159 341836 %s\n", fileName)
+		if got != want {
+			t.Errorf("cli output should be %s, got %s.", want, got)
 		}
 	})
 
@@ -34,4 +75,44 @@ func TestByteCountFile(t *testing.T) {
 		}
 	})
 
+}
+
+func TestLineCountFile(t *testing.T) {
+	t.Run("testing linecount of sample file should return expected value", func(t *testing.T) {
+		fileNames := []string{"../samples/gutenberg.org_cache_epub_132_pg132.txt"}
+		got, err := getLineCountOfFile(fileNames[0])
+		want := 7137
+		if err != nil {
+			t.Errorf("error should be nil, got: %s", err)
+		}
+		if got != want {
+			t.Errorf("count of lines should be %d, got %d", want, got)
+		}
+	})
+}
+func TestWordCountFile(t *testing.T) {
+	t.Run("testing wordcount of sample file should return expected value", func(t *testing.T) {
+		fileNames := []string{"../samples/gutenberg.org_cache_epub_132_pg132.txt"}
+		got, err := getWordCountOfFile(fileNames[0])
+		want := 58159
+		if err != nil {
+			t.Errorf("error should be nil, got: %s", err)
+		}
+		if got != want {
+			t.Errorf("count of words should be %d, got %d", want, got)
+		}
+	})
+}
+func TestCharCountFile(t *testing.T) {
+	t.Run("testing char count of sample file should return expected value", func(t *testing.T) {
+		fileNames := []string{"../samples/gutenberg.org_cache_epub_132_pg132.txt"}
+		got, err := getCharCountOfFile(fileNames[0])
+		want := 339120
+		if err != nil {
+			t.Errorf("error should be nil, got: %s", err)
+		}
+		if got != want {
+			t.Errorf("count of characters should be %d, got %d", want, got)
+		}
+	})
 }
