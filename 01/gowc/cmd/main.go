c1c60533e6821d09453be69c6ff75eaf7b2878d4
Ninad Bhangui <b.ninad50@gmail.com>
2023-08-19 09:02:13 +0530

    added another method which is more performant but incorrect for few use cases

--- 01/gowc/cmd/main.go
+++ 01/gowc/cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 type CliOptions struct {
@@ -55,7 +56,7 @@ func cliEntryPoint(cliOptions *CliOptions, output io.Writer) error {
 	if len(cliOptions.fileNames) == 0 {
 		//Stdin mode
 		cliForSingleFile(cliOptions, os.Stdin, output)
-        fmt.Fprintf(output, " \n")
+		fmt.Fprintf(output, " \n")
 		return nil
 	}
 	for i := range cliOptions.fileNames {
@@ -88,21 +89,25 @@ func cliForSingleFile(cliOptions *CliOptions, input io.Reader, output io.Writer)
 	if cliOptions.shouldGetLineCount {
 		lineCount, err = getSplitCount(reader, bufio.ScanLines)
 		fmt.Fprintf(output, " %d", lineCount)
+		reader = nil
 	}
 	reader = bytes.NewReader(data)
 	if cliOptions.shouldGetWordCount {
 		wordCount, err = getSplitCount(reader, bufio.ScanWords)
 		fmt.Fprintf(output, " %d", wordCount)
+		reader = nil
 	}
 	reader = bytes.NewReader(data)
 	if cliOptions.shouldGetCharCount {
 		charCount, err = getSplitCount(reader, bufio.ScanRunes)
 		fmt.Fprintf(output, " %d", charCount)
+		reader = nil
 	}
 	reader = bytes.NewReader(data)
 	if cliOptions.shouldGetByteCount {
 		byteCount, err = getSplitCount(reader, bufio.ScanBytes)
 		fmt.Fprintf(output, " %d", byteCount)
+		reader = nil
 	}
 	if err != nil {
 		return err
@@ -110,7 +115,69 @@ func cliForSingleFile(cliOptions *CliOptions, input io.Reader, output io.Writer)
 	return nil
 
 }
+func filterEmptyStrings(words []string) []string {
+	finalWords := []string{}
+	for _, word := range words {
+		if word != "" {
+			finalWords = append(finalWords, word)
+		}
+	}
+	return finalWords
 
+}
+func getWordCountInLine(line string) int {
+	count := 0
+	splitLine := strings.FieldsFunc(line, func(r rune) bool {
+		return r == ' ' || r == '\t'
+	})
+	count += len(filterEmptyStrings(splitLine))
+	return count
+}
+
+// TODO: Fix issues. This should be more performant than previous implementation. However it has issues with some special characters that take more bytes
+func cliForSingleFileNew(cliOptions *CliOptions, input io.Reader, output io.Writer) error {
+	scanner := bufio.NewScanner(input)
+	lineCount, wordCount, charCount, byteCount := 0, 0, 0, 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineCount++
+
+		bytes := []byte(line)
+		byteCount += len(bytes)
+
+		// bytes := scanner.Bytes()
+		// byteCount += len(bytes)
+
+		// including \n in bytecount
+		// byteCount += 1
+
+		wordCount += getWordCountInLine(line)
+
+		charCount += len(line) + 1
+
+		// including \n in charCount
+		// charCount += 1
+	}
+
+	//Adding charCount and byteCount for newline character
+	charCount += lineCount - 1
+	byteCount += lineCount - 1
+
+	if cliOptions.shouldGetLineCount {
+		fmt.Fprintf(output, " %d", lineCount)
+	}
+	if cliOptions.shouldGetWordCount {
+		fmt.Fprintf(output, " %d", wordCount)
+	}
+	if cliOptions.shouldGetByteCount {
+		fmt.Fprintf(output, " %d", byteCount)
+	}
+	if cliOptions.shouldGetCharCount {
+		fmt.Fprintf(output, " %d", charCount)
+	}
+
+	return nil
+}
 func getSplitCount(input io.Reader, splitFunc bufio.SplitFunc) (int, error) {
 	count := 0
 	scanner := bufio.NewScanner(input)
--- 01/gowc/cmd/main_test.go
+++ 01/gowc/cmd/main_test.go
@@ -9,13 +9,7 @@ import (
 	"testing"
 )
 
-func TestCli(t *testing.T) {
-	// assertCorrectMessage := func(t testing.TB, got, want string) {
-	// 	t.Helper()
-	// 	if got != want {
-	// 		t.Errorf("got %q want %q", got, want)
-	// 	}
-	// }
+func TestCliWcBytes(t *testing.T) {
 	t.Run("testing bytecount of sample file return same output as wc -c fileName", func(t *testing.T) {
 		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
 		fileNames := []string{fileName}
@@ -40,103 +34,7 @@ func TestCli(t *testing.T) {
 			t.Errorf("cli output should be %s, got %s.", want, got)
 		}
 	})
-	t.Run("testing linecount of sample file return same output as wc -l fileName", func(t *testing.T) {
-		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
-		fileNames := []string{fileName}
-		cliOptions := CliOptions{
-			shouldGetByteCount: false,
-			shouldGetLineCount: true,
-			shouldGetWordCount: false,
-			shouldGetCharCount: false,
-			fileNames:          fileNames,
-		}
-		var buffer bytes.Buffer
-		testWriter := bufio.NewWriter(&buffer)
-		error := cliEntryPoint(&cliOptions, testWriter)
-		if error != nil {
-			t.Errorf("error should be nil, got: %s", error)
-		}
-		testWriter.Flush()
-		got := buffer.String()
-		want := fmt.Sprintf("7137 %s\n", fileName)
-		//TODO: space seperation does not match exactly with the wc command
-		if strings.Trim(got, " ") != want {
-			t.Errorf("cli output should be %s, got %s.", want, got)
-		}
-	})
-	t.Run("testing wordcount of sample file return same output as wc -w fileName", func(t *testing.T) {
-		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
-		fileNames := []string{fileName}
-		cliOptions := CliOptions{
-			shouldGetByteCount: false,
-			shouldGetLineCount: false,
-			shouldGetWordCount: true,
-			shouldGetCharCount: false,
-			fileNames:          fileNames,
-		}
-		var buffer bytes.Buffer
-		testWriter := bufio.NewWriter(&buffer)
-		error := cliEntryPoint(&cliOptions, testWriter)
-		if error != nil {
-			t.Errorf("error should be nil, got: %s", error)
-		}
-		testWriter.Flush()
-		got := buffer.String()
-		want := fmt.Sprintf("58159 %s\n", fileName)
-		//TODO: space seperation does not match exactly with the wc command
-		if strings.Trim(got, " ") != want {
-			t.Errorf("cli output should be %s, got %s.", want, got)
-		}
-	})
-	t.Run("testing charcount of sample file return same output as wc -m fileName", func(t *testing.T) {
-		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
-		fileNames := []string{fileName}
-		cliOptions := CliOptions{
-			shouldGetByteCount: false,
-			shouldGetLineCount: false,
-			shouldGetWordCount: false,
-			shouldGetCharCount: true,
-			fileNames:          fileNames,
-		}
-		var buffer bytes.Buffer
-		testWriter := bufio.NewWriter(&buffer)
-		error := cliEntryPoint(&cliOptions, testWriter)
-		if error != nil {
-			t.Errorf("error should be nil, got: %s", error)
-		}
-		testWriter.Flush()
-		got := buffer.String()
-		want := fmt.Sprintf("339120 %s\n", fileName)
-		//TODO: space seperation does not match exactly with the wc command
-		if strings.Trim(got, " ") != want {
-			t.Errorf("cli output should be %s, got %s.", want, got)
-		}
-	})
 
-	t.Run("testing no options of sample file should behave as -c -l and -w", func(t *testing.T) {
-		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
-		fileNames := []string{fileName}
-		cliOptions := CliOptions{
-			shouldGetByteCount: false,
-			shouldGetLineCount: false,
-			shouldGetWordCount: false,
-			shouldGetCharCount: false,
-			fileNames:          fileNames,
-		}
-		var buffer bytes.Buffer
-		testWriter := bufio.NewWriter(&buffer)
-		error := cliEntryPoint(&cliOptions, testWriter)
-		if error != nil {
-			t.Errorf("error should be nil, got: %s", error)
-		}
-		testWriter.Flush()
-		got := buffer.String()
-		want := fmt.Sprintf("  7137 58159 341836 %s\n", fileName)
-		//TODO: space seperation does not match exactly with the wc command
-		if strings.Trim(got, " ") != strings.Trim(want, " ") {
-			t.Errorf("cli output should be %s, got %s.", want, got)
-		}
-	})
 	t.Run("testing bytecount of stdin return same output as wc -c fileName", func(t *testing.T) {
 		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
 		fileNames := []string{}
@@ -163,4 +61,132 @@ func TestCli(t *testing.T) {
 		}
 	})
 
+}
+func TestCliWcLine(t *testing.T) {
+	t.Run("testing linecount of sample file return same output as wc -l fileName", func(t *testing.T) {
+		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
+		fileNames := []string{fileName}
+		cliOptions := CliOptions{
+			shouldGetByteCount: false,
+			shouldGetLineCount: true,
+			shouldGetWordCount: false,
+			shouldGetCharCount: false,
+			fileNames:          fileNames,
+		}
+		var buffer bytes.Buffer
+		testWriter := bufio.NewWriter(&buffer)
+		error := cliEntryPoint(&cliOptions, testWriter)
+		if error != nil {
+			t.Errorf("error should be nil, got: %s", error)
+		}
+		testWriter.Flush()
+		got := buffer.String()
+		want := fmt.Sprintf("7137 %s\n", fileName)
+		//TODO: space seperation does not match exactly with the wc command
+		if strings.Trim(got, " ") != want {
+			t.Errorf("cli output should be %s, got %s.", want, got)
+		}
+	})
+
+}
+func TestCliWcWord(t *testing.T) {
+	t.Run("testing wordcount of sample file return same output as wc -w fileName", func(t *testing.T) {
+		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
+		fileNames := []string{fileName}
+		cliOptions := CliOptions{
+			shouldGetByteCount: false,
+			shouldGetLineCount: false,
+			shouldGetWordCount: true,
+			shouldGetCharCount: false,
+			fileNames:          fileNames,
+		}
+		var buffer bytes.Buffer
+		testWriter := bufio.NewWriter(&buffer)
+		error := cliEntryPoint(&cliOptions, testWriter)
+		if error != nil {
+			t.Errorf("error should be nil, got: %s", error)
+		}
+		testWriter.Flush()
+		got := buffer.String()
+		want := fmt.Sprintf("58159 %s\n", fileName)
+		//TODO: space seperation does not match exactly with the wc command
+		if strings.Trim(got, " ") != want {
+			t.Errorf("cli output should be %s, got %s.", want, got)
+		}
+	})
+
+}
+func TestCliWcChar(t *testing.T) {
+	t.Run("testing charcount of sample file return same output as wc -m fileName", func(t *testing.T) {
+		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
+		fileNames := []string{fileName}
+		cliOptions := CliOptions{
+			shouldGetByteCount: false,
+			shouldGetLineCount: false,
+			shouldGetWordCount: false,
+			shouldGetCharCount: true,
+			fileNames:          fileNames,
+		}
+		var buffer bytes.Buffer
+		testWriter := bufio.NewWriter(&buffer)
+		error := cliEntryPoint(&cliOptions, testWriter)
+		if error != nil {
+			t.Errorf("error should be nil, got: %s", error)
+		}
+		testWriter.Flush()
+		got := buffer.String()
+		want := fmt.Sprintf("339120 %s\n", fileName)
+		//TODO: space seperation does not match exactly with the wc command
+		if strings.Trim(got, " ") != want {
+			t.Errorf("cli output should be %s, got %s.", want, got)
+		}
+	})
+
+}
+func TestCliDefault(t *testing.T) {
+	t.Run("testing no options of sample file should behave as -c -l and -w", func(t *testing.T) {
+		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
+		fileNames := []string{fileName}
+		cliOptions := CliOptions{
+			shouldGetByteCount: false,
+			shouldGetLineCount: false,
+			shouldGetWordCount: false,
+			shouldGetCharCount: false,
+			fileNames:          fileNames,
+		}
+		var buffer bytes.Buffer
+		testWriter := bufio.NewWriter(&buffer)
+		error := cliEntryPoint(&cliOptions, testWriter)
+		if error != nil {
+			t.Errorf("error should be nil, got: %s", error)
+		}
+		testWriter.Flush()
+		got := buffer.String()
+		want := fmt.Sprintf("  7137 58159 341836 %s\n", fileName)
+		//TODO: space seperation does not match exactly with the wc command
+		if strings.Trim(got, " ") != strings.Trim(want, " ") {
+			t.Errorf("cli output should be %s, got %s.", want, got)
+		}
+	})
+
+}
+func BenchmarkCli(b *testing.B) {
+	b.ResetTimer()
+	count := 1000
+	for i := 0; i < count; i++ {
+		fileName := "../samples/gutenberg.org_cache_epub_132_pg132.txt"
+		fileNames := []string{fileName}
+		cliOptions := CliOptions{
+			shouldGetByteCount: false,
+			shouldGetLineCount: false,
+			shouldGetWordCount: false,
+			shouldGetCharCount: false,
+			fileNames:          fileNames,
+		}
+		var buffer bytes.Buffer
+		testWriter := bufio.NewWriter(&buffer)
+		cliEntryPoint(&cliOptions, testWriter)
+		testWriter.Flush()
+
+	}
 }
